a5bcbc074144ace745b912e37722f8ae9a1e4ccb
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2025-02-16 13:22:22 +0100

    fix redirect

--- src/components/common/sign-in-with-google.tsx
+++ src/components/common/sign-in-with-google.tsx
@@ -1,38 +1,35 @@
-'use client'
-import { authClient } from '@/lib/auth-client'
-import { Loader2 } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { Button } from '../ui/button'
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../ui/button";
 
 const SignInWithGoogle = () => {
-	const [isLoading, setIsLoading] = useState(false)
-	const router = useRouter()
-	const handleSignInWithGoogle = async () => {
-		setIsLoading(true)
-		await authClient.signIn.social(
-			{
-				provider: 'google',
-			},
-			{
-				onSuccess: async () => {
-					router.push('/')
-					router.refresh()
-					setIsLoading(false)
-				},
-			}
-		)
-	}
-	return (
-		<Button
-			className='w-full'
-			variant='outline'
-			onClick={handleSignInWithGoogle}
-			disabled={isLoading}
-		>
-			{isLoading && <Loader2 className='mr-2 animate-spin' />}Login with Google
-		</Button>
-	)
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSignInWithGoogle = async () => {
+    setIsLoading(true);
+    await authClient.signIn.social(
+      {
+        provider: "google",
+        callbackURL: "/wizard",
+      },
+      {
+        onSuccess: async () => {
+          setIsLoading(false);
+        },
+      },
+    );
+  };
+  return (
+    <Button
+      className="w-full"
+      variant="outline"
+      onClick={handleSignInWithGoogle}
+      disabled={isLoading}
+    >
+      {isLoading && <Loader2 className="mr-2 animate-spin" />}Login with Google
+    </Button>
+  );
+};
 
-export default SignInWithGoogle
+export default SignInWithGoogle;
